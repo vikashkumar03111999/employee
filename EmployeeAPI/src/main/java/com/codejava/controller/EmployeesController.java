e60e39fe9af6b94c2a125b2689bcb0ae5d68d8c0
Vikash Kumar Jaiswal <vikashkumar03111999@gmail.com>
2023-09-03 11:46:32 +0530

    Update EmployeesController.java

--- EmployeeAPI/src/main/java/com/codejava/controller/EmployeesController.java
+++ EmployeeAPI/src/main/java/com/codejava/controller/EmployeesController.java
@@ -38,6 +38,51 @@ public class EmployeesController {
 		
 		
 	}
+
+	@GetMapping("/employees/{empid}")
+	public ResponseEntity<Employees> getEmployeesId(@PathVariable long empid){
+		Optional<Employees> emp = employeesRepository.findById(empid);
+		if (emp.isPresent()) {
+			return new ResponseEntity<Employees>(emp.get(),HttpStatus.FOUND);
+		}
+		else {
+			return new ResponseEntity<Employees>(HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	@PutMapping("/employees/{empid}")
+	public String updateEmployeesById(@PathVariable long empid, @RequestBody Employees employees) {
+		Optional<Employees> emp = employeesRepository.findById(empid);
+		if(emp.isPresent()) {
+			Employees existEmp = emp.get();
+			existEmp.setEmpid(employees.getEmpid());
+			existEmp.setEmp_name(employees.getEmp_name());
+			existEmp.setEmp_salary(employees.getEmp_salary());
+			existEmp.setEmp_age(employees.getEmp_age());
+			existEmp.setEmp_city(employees.getEmp_age());
+			employeesRepository.save(existEmp);
+			return "Employees Details against Id " + empid +" Updated ";
+			
+			
+		}
+		else {
+			return "Employees Details does not exist for stuid  " + empid;
+		}
+	}
+	@DeleteMapping("/employees/{empid}")
+	public String deleteEmployeesByEmpId(@PathVariable Long empid) {
+		employeesRepository.deleteById(empid);
+		return "Employees Deleted Successfully";
+	}
+	@DeleteMapping ("/employees")
+	public String deleteAllEmployees() {
+		employeesRepository.deleteAll();
+		return "Employees deleted Successfully...";
+	}
+}
+	
+	
+
 	
 	
 }
